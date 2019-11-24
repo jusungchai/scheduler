caaa0a09e4ea06ebe79162423d49a8e39e3c75b5
Jay <chunsaa@gmail.com>
2019-11-24 00:03:43 -0500

    add components

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import "./styles.scss";
+
+export default function Appointment(props) {
+  console.log(props);
+  return (
+    <article className="appointment"></article>
+  );
+}
