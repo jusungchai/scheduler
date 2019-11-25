cd9b24939e9a74412d3f7d8462d97b2b09f9b1ec
Jay <chunsaa@gmail.com>
2019-11-25 13:12:12 -0500

    change to exports default

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,4 +1,4 @@
-export function getAppointmentsForDay(state, day) {
+export default function getAppointmentsForDay(state, day) {
   const appointmentsForDay = [];
   const dayData = state.days.filter(d => d.name === day);
   console.log(dayData);
