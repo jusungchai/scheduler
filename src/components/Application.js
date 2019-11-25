82ec9e2e212877e70cd56927c52261c1e7b33a88
Jay <chunsaa@gmail.com>
2019-11-25 11:28:02 -0500

    add managing state

--- src/components/Application.js
+++ src/components/Application.js
@@ -41,14 +41,21 @@ const appointments = [
 ];
 
 export default function Application(props) {
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+  /* const [days, setDays] = useState([]);
   const [day, setDay] = useState("Monday");
-  console.log(day);
+  console.log(day); */
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days }));;
 
   useEffect(() => {
     axios.get("/api/days")
       .then(response => {
-        console.log(response);
+        console.log("yo,", response);
         setDays(response.data);
       })
   }, []);
@@ -63,7 +70,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} day={day} setDay={setDay} />
+          <DayList days={state.days} day={state.day} setDay={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
