82b960dc0fbb0cf9c8429f654a85c4aee7d7ca9d
Jay <chunsaa@gmail.com>
2019-11-25 20:21:04 -0500

    add visual mode to appointment component to display form component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,13 +5,36 @@ import "./styles.scss";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import useVisualMode from "hooks/useVisualMode";
+import Form from "components/Appointment/Form";
 
 export default function Appointment(props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   console.log(props);
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+          onCancel={() => back()}
+        />
+      )}
+      {mode === CREATE && (
+        <Form 
+          interviewers={[]}
+          onCancel={() => back()}
+        />
+      )}
     </article>
   );
 }
