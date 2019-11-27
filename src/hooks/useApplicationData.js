d1d3de2fdcb3a1f95a9bc33866918980409fc802
Jay <chunsaa@gmail.com>
2019-11-27 12:34:26 -0500

    add websocket connection

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -33,13 +33,15 @@ export default function useApplicationData() {
   const setDay = day => dispatch({ type: SET_DAY, day });
 
   useEffect(() => {
+    console.log(process.env)
+    const webSocket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
     const daysData = axios.get("/api/days");
     const appointmentsData = axios.get("/api/appointments");
     const interviewersData = axios.get("/api/interviewers");
     Promise.all([daysData, appointmentsData, interviewersData])
       .then(all => {
         dispatch({ type: SET_APPLICATION_DATA, days: all[0].data, appointments: all[1].data, interviewers: all[2].data });
-      })
+      })    
   }, []);
 
   function bookInterview(id, interview) {
