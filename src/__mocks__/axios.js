d94d3724656f9542ee4a9f79260483fc84f6caf6
Jay <chunsaa@gmail.com>
2019-11-29 22:54:37 -0500

    add tests for Application component along with mock axios calls

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -79,5 +79,13 @@ export default {
         data: fixtures.interviewers
       });
     }
+  }),
+  put: jest.fn(url => {
+    if (url === "/api/appointments/1" || url === "/api/appointments/2") {
+      return Promise.resolve({
+        status: 204,
+        statusText: "No Conent",
+      });
+    }
   })
 }
